6e8cc2a975cb485849f9864aa95598bd5e81f8ef
kylcolin <colin@drep.org>
2024-02-26 12:02:09 +0800

    udpate api provider url

--- app.config.js
+++ app.config.js
@@ -11,72 +11,72 @@ const appConfig = {
         // list of providers. if one of them is unavailable, another is randomly picked
         providers: [
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
@@ -93,7 +93,7 @@ const appConfig = {
         // list of providers. if one of them is unavailable, another is randomly picked
         providers: [
             {
-                http: 'https://apiserver-testnet.drep.org',
+                http: 'https://apiserver-testnet.drep.org/api',
                 // for subscriptions
                 ws: '',
             },
